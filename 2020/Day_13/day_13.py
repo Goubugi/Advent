276f21cd860744380b3f4ffa2c15f9d3738aa436
David <dfan1315@gmail.com>
2021-07-29 17:17:31 -0700

    day 13 done

--- 2020/Day_13/day_13.py
+++ 2020/Day_13/day_13.py
@@ -1,4 +1,4 @@
-f = open('sample.txt.txt')
+f = open('input.txt')
 data = f.readlines()
 partOne = False
 
@@ -10,8 +10,20 @@ def findBuses(data):
             ret.append(int(i))
     return ret
 
+def findBuses2(data):
+    ret = []
+    data = data.split(',')
+    for i in data:
+        if i != 'x':
+            ret.append(int(i))
+        else:
+            ret.append(i)
+    return ret
+
 earliest = int(data[0][:-1])
 buses = findBuses(data[1])
+
+buses2 = findBuses2(data[1])
 
 def findEarliestBus():
     current = earliest
@@ -21,6 +33,33 @@ def findEarliestBus():
                 return (current-earliest)*i
         current+=1
 
-answer1 = findEarliestBus()
-print(answer1)
+def findEarliestPattern():
+    count = 0
+    skip = buses[0]
+    added = [buses[0]]
+    while True:
+        index = 0
+        for i in buses2:
+            if i == 'x':
+                pass
+            else:
+                if (count+index)%i==0:
+                    if i not in added:
+                        added.append(i)
+                        skip *= i
+                    if i == buses2[-1]:
+                        return count
+                else:
+                    break
+            index+=1
+        count+=skip
+
+
+
+if partOne:
+    answer1 = findEarliestBus()
+    print(answer1)
+else:
+    answer2 = findEarliestPattern()
+    print(answer2)
 
